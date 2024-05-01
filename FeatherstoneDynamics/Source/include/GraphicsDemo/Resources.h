a76d577903b987df6e52415d64b972e66e2658ec
Zain Khan <zain.9496@gmail.com>
2024-05-01 05:13:32 +0500

    Added simple graphics functionality with Cinder

---
+++ FeatherstoneDynamics/Source/include/GraphicsDemo/Resources.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_VERT_SHADER		CINDER_RESOURCE( ../../Source/shaders/, vertex.glsl, 128, GLSL )
+#define RES_FRAG_SHADER		CINDER_RESOURCE( ../../Source/shaders/, fragment.glsl, 129, GLSL )
+
--- FeatherstoneDynamics/Source/src/GraphicsDemo/main.cpp
+++ FeatherstoneDynamics/Source/src/GraphicsDemo/main.cpp
@@ -1,25 +1,98 @@
-#include <SFML/Graphics.hpp>
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Log.h"
+#include "GraphicsDemo/Resources.h"
+
+using namespace ci;
+using namespace ci::app;
+
+class RotatingCubeApp : public App {
+public:
+	void	setup() override;
+	void	resize() override;
+	void	update() override;
 
 
-int main()
+
+	void	DrawCube();
+	void	DrawPlane();
+	void	draw() override;
+
+	CameraPersp			mCam;
+	gl::BatchRef		mCube;
+	gl::BatchRef		mPlane;
+	gl::GlslProgRef		mGlsl;
+	mat4				mCubeRotation;
+
+	vec3				mCubePos;
+	vec3				mPlanePos;
+
+	double				ang = 0.0;
+};
+
+void RotatingCubeApp::setup()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	try {
+		mGlsl = gl::GlslProg::create(loadResource(RES_VERT_SHADER), loadResource(RES_FRAG_SHADER));
+		CI_LOG_I("Loaded shader");
+	}
+	catch (const std::exception& e) {
+		CI_LOG_E("Shader Error: " << e.what());
+	}
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	mCube = gl::Batch::create(geom::Cube().size(vec3(1, 1, 1)), mGlsl);
+	mPlane = gl::Batch::create(geom::Plane().size(vec2(10, 10)).normal(vec3(0, 0, 1)), mGlsl);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	mPlanePos = vec3(0, 0, 0);
+	mCubePos = vec3(0, 0, 2);
 
-    return 0;
+	gl::enableDepthWrite();
+	gl::enableDepthRead();
 }
+
+void RotatingCubeApp::resize()
+{
+	mCam.setPerspective(60, getWindowAspectRatio(), 1, 1000);
+	mCam.lookAt(vec3(5, 2, 3), vec3(0), vec3(0, 0, 1));
+	gl::setMatrices(mCam);
+}
+
+void RotatingCubeApp::update()
+{
+	//mCubeRotation *= rotate(toRadians(0.2f), normalize(vec3(0, 0, 1)));
+}
+
+void RotatingCubeApp::draw()
+{
+	gl::clear();
+
+	gl::setMatrices(mCam);
+	
+	// Draw Cube
+	DrawCube();
+
+	// Draw Plane
+	DrawPlane();
+	ang += 0.01;
+}
+
+void RotatingCubeApp::DrawCube()
+{
+	gl::pushModelMatrix();
+	gl::translate(mCubePos + vec3(0, 0, sin(ang)));
+	gl::color(Color(1.0f, 0.0f, 0.0f));
+	mCube->draw();
+	gl::popModelMatrix();
+}
+void RotatingCubeApp::DrawPlane()
+{
+	gl::pushModelMatrix();
+	gl::translate(mPlanePos);
+	gl::color(Color(0.0f, 0.0f, 1.0f));
+	mPlane->draw();
+	gl::popModelMatrix();
+}
+
+CINDER_APP(RotatingCubeApp, RendererGl(RendererGl::Options().msaa(16)))
